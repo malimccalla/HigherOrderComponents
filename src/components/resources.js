74dc6b6674ac9c6f7ccfae7651866ef195d41559
Mali Michael <malimccalla@gmail.com>
2016-10-16 11:30:42 +0100

    simple auth

---
+++ src/components/resources.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super special secret recipe
+      <li>Ingredient 1</li>
+      <li>Ingredient 2</li>
+      <li>Ingredient 3</li>
+    </div>
+  );
+};
